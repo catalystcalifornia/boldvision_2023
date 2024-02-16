9eca23cddbfc8daf5f13256a337fe22ac10a8255
Elycia Mulholland Graves <egraves@advanceproj.org>
2024-02-16 13:21:19 -0800

    small changes to the chart function template script and add a visual folder with a yp_baseorgs test script

--- Visuals/Youth Power/yp_baseorgs.R
+++ Visuals/Youth Power/yp_baseorgs.R
@@ -8,15 +8,14 @@ source("bv_barchart_function.R")
 source("W:\\RDA Team\\R\\credentials_source.R")
 con <- connect_to_db("bold_vision")
 
-#this TEMPLATE example will use the connected youth df
-df_subgroup <- st_read(con, query = "select * from bv_2023.pyd_connectedyouth_subgroup")
+df_subgroup <- st_read(con, query = "select * from bv_2023.yp_baseorgs_subgroup")
 
 #pull race labels
 race_label_df <- st_read(con, query = "select * from bv_2023.metadata_race_labels")
 
 ####Step 2: join your table to the metadata table to get the correct labels ####
 #join to your table if needed rename subgroup to race
-# df_subgroup<-df_subgroup%>%rename(race=subgroup) # comment out this line if not needed
+df_subgroup<-df_subgroup%>%rename(race=subgroup) # comment out this line if not needed
 
 #Please do NOT rename dataframe tables as the function will pull from them. 
 df_subgroup <- left_join(df_subgroup, race_label_df, by=c("race" = "race_base"))  %>%
@@ -30,20 +29,20 @@ df <- subset(df_subgroup, race != "total" & race != "bipoc") %>%
 fx_barchart_subgroup(
   df = df,
   #be sure to write in the domain this way so it reflects the correct folders that the function will insert the visual deliverables in. 
-  domain = "Positive Youth Development",
-  indicator = "Connected Youth",
+  domain = "Youth Power",
+  indicator = "Access to Base-Building Organizations",
   # insert a findings based systems led title
-  title = "Educational Institutions and Employers are less likely to engage with Black, NHPI, and Latine Youth",
+  title = "Social Advocacy and Base-Building Organizations Are Less Available to Asian and NHPI Youth",
   #explanation of what the we are looking at // use sentence case 
-  subtitle = "Percent of connected youth by race in Los Angeles County",
+  subtitle = "Average percentile access to base-building organizations by youth race in Los Angeles County",
   #please follow the format of the datasource below
-  caption_datasource = "Catalyst California's calculations based on American Community Survey Public Use Microdata, 2017-2021 5-Year Estimates.",
+  caption_datasource = "Catalyst California's calculations based on the IRS Business Master File Extract (2023), CauseIQ (2022), and U.S. Census Bureau, Detailed Housing and Demographic Characteristics (2020).",
   #only input the full names for the groups that are in acronyms and do NOT modify this racenote unless necessary for their inidicator
-  caption_racenote = "AIAN=American Indian or Alaska Native; NHPI=Native Hawaiian or Pacific Islander; SWANA=Southwest Asian or North African/Middle Eastern or North African; Another Race=Persons who identify with a racial group not presented",
+  caption_racenote = "AIAN=American Indian or Alaska Native; NHPI=Native Hawaiian or Pacific Islander; Another Race=Persons who identify with a racial group not presented",
   #define the indicator
-  caption_indicator_def = "Connected youth are defined as youth (ages 14-24) who are enrolled in school or are employed.",
+  caption_indicator_def = "Average percentile (%ile) rate is based on census tract characteristics and indicates the average access each youth racial group (ages 0-24) has to social advocacy or base-building organizations serving youth in their area. A higher percentile indicates higher access.",
   #define the unit of the data and remember to use quotations (i.e. "%" or "per 1k") 
-  data_unit = "%"
+  data_unit = "%ile"
 )
 
 dbDisconnect(conn = con)
