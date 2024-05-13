3b6ffe1e81b5061a0fae8fa265ee87aadf76d4ab
Elycia Mulholland Graves <egraves@advanceproj.org>
2024-05-13 13:18:45 -0700

    rerunning YP visuals for updated fonts and slight modifications to captions and subtitles

--- Visuals/Youth Power/yp_youth_voter_turnout.R
+++ Visuals/Youth Power/yp_youth_voter_turnout.R
@@ -9,7 +9,8 @@ source("W:\\RDA Team\\R\\credentials_source.R")
 con <- connect_to_db("bold_vision")
 
 #this TEMPLATE example will use the connected youth df
-df_subgroup <- st_read(con, query = "select * from bv_2023.yp_civic_engage_youth_subgroup")
+df_subgroup <- st_read(con, query = "select * from bv_2023.yp_youth_voter_turnout_subgroup")
+df_subgroup$subgroup <- gsub("Total", "total", df_subgroup$subgroup)
 
 #pull race labels
 race_label_df <- st_read(con, query = "select * from bv_2023.metadata_race_labels")
@@ -31,17 +32,17 @@ fx_barchart_subgroup(
   df = df,
   #be sure to write in the domain this way so it reflects the correct folders that the function will insert the visual deliverables in. 
   domain = "Youth Power",
-  indicator = "Civic Engagement",
+  indicator = "Voter Turnout",
   # insert a findings based systems led title
-  title = "Engagement with politics in their community is least likely among Asian and Latine youth",
+  title = "Voting in elections is least likely among Asian and Latine youth",
   #explanation of what the we are looking at // use sentence case 
-  subtitle = "Percent of civically engaged youth in Los Angeles County",
-  #please follow the format of the datasource below
-  caption_datasource = "Catalyst California's calculations of IPUMS CPS Civic Engagement Supplement, pooled from 2013, 2017, 2019, and 2021.",# Data for 2022 and 2023 did not include data by racial and ethnic group so was not included in the analysis.
+  subtitle = "Percent of eligible youth voter turnout in Los Angeles County",
+  #please follow the format of the data source below
+  caption_datasource = "Catalyst California's calculations of IPUMS Current Population Survey Voting Supplement, pooled estimates from 2014, 2016, 2018, 2020.",
   #only input the full names for the groups that are in acronyms and do NOT modify this racenote unless necessary for their indicator
   caption_racenote = "AIAN=American Indian or Alaska Native; NHPI=Native Hawaiian or Pacific Islander. NHPI estimates are unstable",
   #define the indicator
-  caption_indicator_def = "Civically engaged youth are defined as youth ages 18-29 who report engaging or participating in politics (e.g., participating in boycotts, serving on a committe of an organization, participating in a service or civic organization, etc.).",
+  caption_indicator_def = "Eligible youth voter turnout is defined as youth citizens, ages 18-29 years, who voted during presidential and midterm elections.",
   #define the unit of the data and remember to use quotations (i.e. "%" or "per 1k") 
   data_unit = "%"
 )
